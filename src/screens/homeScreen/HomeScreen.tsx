a413b4afbbf5eeb02b61f9d4b606202744f0a21a
ben <benfherm@gmail.com>
2024-07-02 23:41:07 +0300

    test commit

--- src/screens/homeScreen/HomeScreen.tsx
+++ src/screens/homeScreen/HomeScreen.tsx
@@ -7,6 +7,7 @@ import {RootStackParamList} from '../../navigation/interfaces';
 import {useFetchPackages} from './hooks/useFetchPackages';
 import {routes} from '../../navigation/constants';
 
+//test
 type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
 
 const HomeScreen: React.FC = () => {
